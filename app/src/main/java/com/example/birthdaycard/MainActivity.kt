5a5e1dd10aed5e566e560ab6174b41bf2f3cf1da
Mubarak92 <Mubarak.Abdullah.ruh.k@tuwaiq.edu.sa>
2021-10-27 15:51:30 +0300

    sound has been added

--- app/src/main/java/com/example/birthdaycard/MainActivity.kt
+++ app/src/main/java/com/example/birthdaycard/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.example.birthdaycard
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button2)
+        rollButton.setOnClickListener { playsound()
+
+        }
+    }
+    private fun playsound() {
+    var sound1= MediaPlayer.create(applicationContext,R.raw.party)
+        sound1?.start()
     }
 
-    fun clickhere(view: View) {
-        mediaController
 
-    }
 }
